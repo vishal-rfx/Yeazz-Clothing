7a2ca771edd052d82a19df94cdc64cc9642f3de0
vishal-rfx <vishal23rfx@gmail.com>
2022-03-15 00:00:48 +0530

    added styles to menu-item

--- src/components/menu-item/menu-item.component.tsx
+++ src/components/menu-item/menu-item.component.tsx
@@ -8,12 +8,13 @@ type MenuProps = {
 };
 
 const MenuItem = ({ title, imageUrl, size }: MenuProps) => (
-  <div
-    style={{ backgroundImage: `url(${imageUrl})` }}
-    className={`menu-item ${size}`}
-  >
+  <div className={`menu-item ${size}`}>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
